6485b07f66c9822d9e3640228b65003bf26ca700
zhangxiang1008 <1045536138@qq.com>
2024-05-31 15:38:20 +0800

    继承实现

--- src/utils/Extends.js
+++ src/utils/Extends.js
@@ -18,3 +18,43 @@ SubObject.prototype.getSubValue = function () {
   return this.subProperty
 }
 // 缺点： 父类引用属性实例的原型会全部暴露给子类、无法给父类构造函数中传参
+
+// ======================2、构造函数继承=================
+
+function SubObject() {
+  SuperObject.call(this)
+  this.subProperty = false
+}
+// 缺点： 无法获取父类的方法，需要自定义方法
+
+// ======================3、混合继承===================
+function SubObject() {
+  SuperObject.call(this)
+  this.subProperty = false
+}
+SubObject.prototype = new SuperObject()
+SubObject.prototype.constructor = SubObject
+//定义自己的方法
+SubObject.prototype.getSubValue = function () {
+  return this.subProperty
+}
+// 缺点：
+
+// ================4、寄生式继承============
+// 使用原型 获取父类函数方法
+function object(target) {
+  function F() {}
+  F.prototype = target
+  return new F()
+}
+function interAliveExtends(SuperObj, SubObj) {
+  const protoType = object(SuperObj.protoType)
+  protoType.constructor = SubObj
+  SubObj.protoType = protoType
+}
+// 调用父类构造函数获取
+function SubObject() {
+  SuperObject.call(this)
+  this.subProperty = false
+}
+interAliveExtends(SuperObject, SubObject)
